6eb366bf9fd6aaaffd3632512f3b1e5ae8c6a78d
AmadejKrepek <krepekamadej@gmail.com>
2023-06-30 13:57:21 +0200

    Added foreign key migration

--- MaximusWebApiCleanArchitecture/Persistence/Migrations/20230630112727_AddedStationIdAndCollection.Designer.cs
+++ MaximusWebApiCleanArchitecture/Persistence/Migrations/20230630112727_AddedStationIdAndCollection.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence.Context;
 
@@ -10,9 +11,11 @@ using Persistence.Context;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(StationContext))]
-    partial class StationContextModelSnapshot : ModelSnapshot
+    [Migration("20230630112727_AddedStationIdAndCollection")]
+    partial class AddedStationIdAndCollection
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
