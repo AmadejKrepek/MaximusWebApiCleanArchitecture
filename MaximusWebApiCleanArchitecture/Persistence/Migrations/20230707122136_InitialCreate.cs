310ef6626ecd00c04f9fe6eb07f829d08b18dff5
AmadejKrepek <krepekamadej@gmail.com>
2023-07-07 14:26:56 +0200

    applied fresh migrations

---
+++ MaximusWebApiCleanArchitecture/Persistence/Migrations/20230707122136_InitialCreate.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Stations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Latitude = table.Column<float>(type: "float", nullable: true),
+                    Longitude = table.Column<float>(type: "float", nullable: true),
+                    Altitude = table.Column<double>(type: "double", nullable: true),
+                    ConnectionStatus = table.Column<int>(type: "int", nullable: false),
+                    LastUpdated = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: true),
+                    UnitOfMeasurement = table.Column<int>(type: "int", nullable: false),
+                    DateCreated = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false),
+                    DateUpdated = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: true),
+                    DateDeleted = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stations", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "StationData",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Temperature = table.Column<double>(type: "double", nullable: true),
+                    Dewpoint = table.Column<double>(type: "double", nullable: true),
+                    Humidity = table.Column<double>(type: "double", nullable: true),
+                    WindSpeed = table.Column<double>(type: "double", nullable: true),
+                    WindGust = table.Column<double>(type: "double", nullable: true),
+                    WindDirection = table.Column<int>(type: "int", nullable: true),
+                    DailyRain = table.Column<double>(type: "double", nullable: true),
+                    SolarRadiation = table.Column<double>(type: "double", nullable: true),
+                    StationId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    DateCreated = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false),
+                    DateUpdated = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: true),
+                    DateDeleted = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StationData", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StationData_Stations_StationId",
+                        column: x => x.StationId,
+                        principalTable: "Stations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StationData_StationId",
+                table: "StationData",
+                column: "StationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StationData");
+
+            migrationBuilder.DropTable(
+                name: "Stations");
+        }
+    }
+}
